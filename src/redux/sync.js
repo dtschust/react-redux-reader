89ef1a39d60268e1be699861fac8b9cf3836c319
Drew Schuster <dtschust@gmail.com>
2017-09-30 20:16:49 -0700

    Prune posts older than 10 days that have been read during sync

--- src/redux/sync.js
+++ src/redux/sync.js
@@ -3,8 +3,31 @@ import _ from 'lodash';
 import { apiFetchFeedItemsByIds, apiFetchAllUnreadIds, apiFetchLastNDaysOfFeedItems } from '../feed-wrangler-api';
 import { updateSyncingState } from './reducers/app-state-store';
 import { fetchSubscriptions } from './reducers/subscriptions-store';
-import { addFeedItems, getAllFeedItemIds, deleteFeedItemsById } from './reducers/feed-items-store';
+import { getAllFeedItems, addFeedItems, getAllFeedItemIds, deleteFeedItemsById } from './reducers/feed-items-store';
 
+function pruneOldReadPosts() {
+	return (dispatch, getState) => {
+		const state = getState();
+		const now = Date.now() / 1000;
+		const tenDaysAgo = now - 10 * 60 * 60 * 24;
+
+		const allFeedItems = getAllFeedItems(state);
+		const idsToRemove = _(allFeedItems)
+			.filter((feedItem) => {
+				if (feedItem.read && feedItem.published_at < tenDaysAgo) {
+					return true;
+				}
+
+				return false;
+			})
+			.map(({ feed_item_id }) => feed_item_id.toString())
+			.value();
+
+		if (idsToRemove.length) {
+			dispatch(deleteFeedItemsById(idsToRemove));
+		}
+	}
+}
 
 export default function sync() {
 	return (dispatch, getState) => {
@@ -44,6 +67,8 @@ export default function sync() {
 				if (deadIds.length) {
 					dispatch(deleteFeedItemsById(deadIds));
 				}
+
+				dispatch(pruneOldReadPosts());
 				dispatch(updateSyncingState(false));
 			})
 		});

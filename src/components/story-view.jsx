2ae5a5fc6c769091ffbc1ba31299304c7ea34435
Drew Schuster <dtschust@gmail.com>
2020-05-18 21:29:07 -0700

    install react-twitter-embed, add it to story view

--- src/components/story-view.jsx
+++ src/components/story-view.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { TwitterTweetEmbed } from 'react-twitter-embed';
 
 import { getSelectedFeedItemId } from '../redux/reducers/app-state-store';
 import { getFeedItem } from '../redux/reducers/feed-items-store';
 
-function StoryView({ body } = {}) {
+function StoryView({ body, url } = {}) {
 	if (!body) {
 		return false;
+	}
+
+	let tweetId;
+	try {
+		if (url.indexOf('https://twitter.com/') === 0) {
+			tweetId = url.match(/\/(\d+)$/)[1];
+		}
+	} catch (e) {
+		// I don't care
 	}
 	return (
 		<div
@@ -15,8 +25,18 @@ function StoryView({ body } = {}) {
 				padding: '50px 20px',
 				lineHeight: '1.5',
 			}}
-			dangerouslySetInnerHTML={{ __html: body }}
-		/>
+		>
+			{tweetId && (
+				<div
+					style={{
+						marginBottom: '50px',
+					}}
+				>
+					<TwitterTweetEmbed key={tweetId} tweetId={tweetId} />
+				</div>
+			)}
+			<div dangerouslySetInnerHTML={{ __html: body }} />
+		</div>
 	);
 }
 

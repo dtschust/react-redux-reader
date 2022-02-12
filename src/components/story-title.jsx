a534cf1dacaf95be6c8a30a4dd727a6b337ac20d
Drew Schuster <dschuster@slack-corp.com>
2022-02-11 19:41:59 -0800

    More html in places

--- src/components/story-title.jsx
+++ src/components/story-title.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import sanitizeHtml from 'sanitize-html';
 
 import { getSelectedFeedItemId } from '../redux/reducers/app-state-store';
 import { getFeedItem } from '../redux/reducers/feed-items-store';
@@ -16,6 +17,13 @@ function StoryTitle({
 	if (!title) {
 		return false;
 	}
+	const sanitizedTitle = sanitizeHtml(title, {
+		allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img', 'iframe' ]),
+		allowedAttributes: {
+			...sanitizeHtml.defaults.allowedAttributes,
+			iframe: ['src', 'width', 'height']
+		},
+	});
 	return (
 		<div className="story-title" style={{ padding: '20px' }}>
 			<div style={{ color: '#919190' }}>
@@ -26,9 +34,8 @@ function StoryTitle({
 					style={{ textDecoration: 'none', fontSize: '42px' }}
 					href={url}
 					target="_blank"
-				>
-					{title}
-				</a>
+					dangerouslySetInnerHTML={{ __html: sanitizedTitle }}
+				/>
 			</h1>
 			<div style={{ color: '#919190' }}>{author}</div>
 			<div style={{ color: '#919190' }}>{feed_name}</div>

f64c3ea747347629c8827fa61a7b4cb8759f4c05
Drew Schuster <dtschust@gmail.com>
2022-12-19 18:22:58 -0800

    untested wip. trying to comment out all api calls and start work on subscriptions only

--- src/feed-wrangler-api.js
+++ src/feed-wrangler-api.js
@@ -14,129 +14,45 @@ export function apiAuth(email, password) {
 	fetch(url, { headers })
 		.then(() => {
 			console.log('success!');
-			// TODO: Store authorization header in localStorage
+			localStorage.setItem('accessToken', 'Basic ' + btoa(email + ":" + password));
 			window.location.reload();
 		});
 }
 
 export function logout() {
-	let url = `${API_ENDPOINT}/users/logout/?access_token=${ACCESS_TOKEN}`;
-	fetch(url)
-		.then(response => response.json())
-		.then(() => {
-			localStorage.removeItem('accessToken');
-			purgePersistor().then(() => {
-				window.location.reload();
-			});
-		});
+	localStorage.removeItem('accessToken');
+	purgePersistor().then(() => {
+		window.location.reload();
+	});
 }
 
 export function apiFetchFeedItems(limit, offset, options = {}) {
-	let url = `${API_ENDPOINT}/feed_items/list?access_token=${ACCESS_TOKEN}`;
-
-	if (limit) {
-		url += `&limit=${limit}`;
-	}
-
-	if (offset) {
-		url += `&offset=${offset}`;
-	}
-
-	if (!_.isUndefined(options.read)) {
-		url += `&read=${options.read}`;
-	}
-
-	if (!_.isUndefined(options.starred)) {
-		url += `&starred=${options.starred}`;
-	}
-
-	if (!_.isUndefined(options.feedId)) {
-		url += `&feed_id=${options.feedId}`;
-	}
-
-	return fetch(url).then(response => response.json());
+	return Promise.resolve([]);
 }
 
 export function apiUpdateFeedItem(id, options = {}) {
-	let url = `${API_ENDPOINT}/feed_items/update?access_token=${ACCESS_TOKEN}&feed_item_id=${id}`;
-
-	if (!_.isUndefined(options.read)) {
-		url += `&read=${options.read}`;
-	}
-
-	return fetch(url, { method: 'post' }).then(response => response.json());
-
 	// Fake version for testing
-	// return Promise.resolve({
-	// 	result: 'success',
-	// });
+	return Promise.resolve({
+		result: 'success',
+	});
 }
 
 export function apiFetchSubscriptions() {
-	const url = `${API_ENDPOINT}/subscriptions/list?access_token=${ACCESS_TOKEN}`;
+	// TODO:
+	return Promise.resolve([]);
+	// const url = `${API_ENDPOINT}/subscriptions/list?access_token=${ACCESS_TOKEN}`;
 
-	return fetch(url).then(response => response.json());
+	// return fetch(url).then(response => response.json());
 }
 
 export function apiFetchFeedItemsByIds(ids) {
-	let url = `${API_ENDPOINT}/feed_items/get?access_token=${ACCESS_TOKEN}`;
-
-	url += `&feed_item_ids=${ids.join(',')}`;
-
-	return fetch(url).then(response =>
-		response.json().then(({ feed_items }) => feed_items),
-	);
+	return Promise.resolve([]);
 }
 
 export function apiFetchAllUnreadIds(offset = 0) {
-	let url = `${API_ENDPOINT}/feed_items/list_ids?access_token=${ACCESS_TOKEN}`;
-
-	url += '&read=false';
-	url += '&limit=1000';
-
-	if (offset > 0) {
-		url += `&offset=${offset}`;
-	}
-
-	return fetch(url)
-		.then(response => response.json())
-		.then(({ feed_items: feedItems }) => {
-			const feedItemIds = _.map(feedItems, ({ feed_item_id }) =>
-				feed_item_id.toString(),
-			);
-			if (feedItemIds.length === 1000) {
-				// fetch more
-				return apiFetchAllUnreadIds(offset + 1000).then(newFeedItems => {
-					return feedItemIds.concat(newFeedItems);
-				});
-			}
-			return feedItemIds;
-		});
+	return Promise.resolve([]);
 }
 
 export function apiFetchLastNDaysOfFeedItems(numDays, offset = 0) {
-	let url = `${API_ENDPOINT}/feed_items/list?access_token=${ACCESS_TOKEN}`;
-
-	url += '&limit=100';
-	url += `&created_since=${Math.floor(Date.now() / 1000) -
-		numDays * 60 * 60 * 24}`;
-
-	if (offset > 0) {
-		url += `&offset=${offset}`;
-	}
-
-	return fetch(url)
-		.then(response => response.json())
-		.then(({ feed_items: feedItems }) => {
-			if (feedItems.length === 100) {
-				// fetch more
-				return apiFetchLastNDaysOfFeedItems(
-					numDays,
-					offset + 100,
-				).then(newFeedItems => {
-					return feedItems.concat(newFeedItems);
-				});
-			}
-			return feedItems;
-		});
+	return Promise.resolve([]);
 }
